1d6d603ceea0b9f1b7b727554c60441faeeb5458
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-10 00:41:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,29 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all << Song.new
+    @@all[-1]
+  end
+
+  def self.new_by_name(name)
+    s = Song.new
+    s.name = name
+    s
+  end
+
+  def self.create_by_name(name)
+    s = Song.new
+    s.name = name
+    @@all << s
+    s
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name) if self.find_by_name(name)
+  end
 end
