375b4611dabe7f4c79aaa864624857a80507e480
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-10 00:43:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) if self.find_by_name(name)
+    if self.find_by_name(name)
+      self.create_by_name(name)
+    end
   end
 end
